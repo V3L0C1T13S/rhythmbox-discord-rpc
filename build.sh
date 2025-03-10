1fde0fb67b3433bc756d84e0192c5f86da17e540
V3L0C1T13S <turtletester20@gmail.com>
2025-03-10 18:33:49 -0400

    update build script

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+export PATH="/usr/lib64/ccache:$PATH"
+export CC=/usr/lib64/ccache/clang CXX=/usr/lib64/ccache/clang++
+export CC_LD=lld CXX_LD=lld
 BUILD_DIRECTORY="./build"
 
 meson setup $BUILD_DIRECTORY
