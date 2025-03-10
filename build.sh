4c267bdd49d96c1702b94f6d50eb41a893c611ad
V3L0C1T13S <turtletester20@gmail.com>
2025-03-10 18:51:18 -0400

    use gcc

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export PATH="/usr/lib64/ccache:$PATH"
-export CC=/usr/lib64/ccache/clang CXX=/usr/lib64/ccache/clang++
+export CC=/usr/lib64/ccache/gcc CXX=/usr/lib64/ccache/gcc++
 export CC_LD=lld CXX_LD=lld
 BUILD_DIRECTORY="./build"
 
